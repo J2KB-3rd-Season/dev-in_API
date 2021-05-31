786f610aa9669c9a5be4db7cb674aa411f7aeae7
enjay27 <kdkm2002@gmail.com>
2021-05-31 13:31:55 +0900

    feat: password 암호화를 위한 bean config 추가

--- src/main/java/com/devin/dev/AppConfiguration.java
+++ src/main/java/com/devin/dev/AppConfiguration.java
@@ -3,15 +3,34 @@ package com.devin.dev;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.EntityManager;
 
 @Configuration
-public class AppConfiguration {
+@EnableWebSecurity
+public class AppConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     JPAQueryFactory jpaQueryFactory(EntityManager em) {
         return new JPAQueryFactory(em);
     }
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()      // cors 비활성화
+                .csrf().disable()      // csrf 비활성화
+                .formLogin().disable() //기본 로그인 페이지 없애기
+                .headers().frameOptions().disable();
+    }
 }
