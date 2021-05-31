9879df94110574888843931fce5424edb2eb826a
enjay27 <kdkm2002@gmail.com>
2021-05-31 14:44:44 +0900

    fix: ResponseMessage 로그인 실패메시지 변경

--- src/main/java/com/devin/dev/utils/ResponseMessage.java
+++ src/main/java/com/devin/dev/utils/ResponseMessage.java
@@ -2,6 +2,8 @@ package com.devin.dev.utils;
 
 public enum ResponseMessage {
     LOGIN_SUCCESS("로그인 성공"),
+    NOT_EXIST_EMAIL("존재하지 않는 이메일"),
+    INCORRECT_PASSWORD("패스워드 오류"),
     LOGIN_FAIL("로그인 실패"),
     READ_USER("회원 정보 조회 성공"),
     NOT_FOUND_USER("회원을 찾을 수 없습니다."),
@@ -9,7 +11,7 @@ public enum ResponseMessage {
     UPDATE_USER("회원 정보 수정 성공"),
     DELETE_USER("회원 탈퇴 성공"),
     INTERNAL_SERVER_ERROR("서버 내부 에러"),
-    EXIST_USER_MAIL("이미 존재하는 회원 메일"),
+    EXIST_USER_EMAIL("이미 존재하는 회원 메일"),
     DB_ERROR("데이터베이스 에러");
 
     ResponseMessage(String message) {
