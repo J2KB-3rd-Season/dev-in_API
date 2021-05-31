d66b804f9f926578a4b42322236997060cae5ac2
enjay27 <kdkm2002@gmail.com>
2021-05-31 13:33:38 +0900

    feat: 클라이언트 응답용 Response 클래스 생성

---
+++ src/main/java/com/devin/dev/model/DefaultResponse.java
@@ -0,0 +1,28 @@
+package com.devin.dev.model;
+
+import com.devin.dev.utils.ResponseMessage;
+import com.devin.dev.utils.StatusCode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+@Getter
+public class DefaultResponse<T> {
+
+    private final StatusCode statusCode;
+    private final ResponseMessage responseMessage;
+    private final T data;
+
+    public DefaultResponse(StatusCode statusCode, ResponseMessage responseMessage) {
+        this.statusCode = statusCode;
+        this.responseMessage = responseMessage;
+        this.data = null;
+    }
+
+    public DefaultResponse(StatusCode statusCode, ResponseMessage responseMessage, T data) {
+        this.statusCode = statusCode;
+        this.responseMessage = responseMessage;
+        this.data = data;
+    }
+}
