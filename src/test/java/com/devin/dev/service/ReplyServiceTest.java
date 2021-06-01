0691311720f7575583020e58b0a0b7288c873212
enjay27 <kdkm2002@gmail.com>
2021-06-01 14:54:59 +0900

    test: 댓글 실패, 좋아요 실패, 답변 수정 실패 테스트케이스 및 기존 테스트에서 response 객체 검증 테스트 추가

--- src/test/java/com/devin/dev/service/ReplyServiceTest.java
+++ src/test/java/com/devin/dev/service/ReplyServiceTest.java
@@ -1,6 +1,7 @@
 package com.devin.dev.service;
 
 import com.devin.dev.dto.reply.ReplyDto;
+import com.devin.dev.dto.reply.ReplyLikeDto;
 import com.devin.dev.entity.post.Post;
 import com.devin.dev.entity.reply.Reply;
 import com.devin.dev.entity.reply.ReplyImage;
@@ -10,6 +11,8 @@ import com.devin.dev.entity.user.UserStatus;
 import com.devin.dev.model.DefaultResponse;
 import com.devin.dev.repository.post.PostRepository;
 import com.devin.dev.repository.reply.ReplyRepository;
+import com.devin.dev.utils.ResponseMessage;
+import com.devin.dev.utils.StatusCode;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -39,7 +42,7 @@ class ReplyServiceTest {
     PostRepository postRepository;
 
     @Test
-    void replyTest() {
+    void replySuccess() {
         User postUser = new User("D", "d@b.com", "passD", "0004", UserStatus.ACTIVE);
         User replyUser = new User("E", "e@b.com", "passE", "0005", UserStatus.ACTIVE);
         Post post1 = new Post(postUser, "PostC1", "ContentC1");
@@ -47,7 +50,10 @@ class ReplyServiceTest {
         em.persist(post1);
         em.persist(replyUser);
 
-        DefaultResponse<ReplyDto> response = replyService.reply(replyUser.getId(), post1.getId(), "reply_content", List.of("i1", "i2", "i3"));
+        DefaultResponse<ReplyDto> response = replyService.reply(replyUser.getId(), post1.getId(),
+                "reply_content", List.of("i1", "i2", "i3"));
+        assertThat(response.getStatusCode()).isEqualTo(StatusCode.OK);
+        assertThat(response.getResponseMessage()).isEqualTo(ResponseMessage.REPLY_UPLOAD_SUCCESS);
 
         Reply foundReply = em.find(Reply.class, response.getData().getId());
 
@@ -64,7 +70,41 @@ class ReplyServiceTest {
     }
 
     @Test
-    void replyLikeTest() {
+    void replyWrongUserFailed() {
+        User postUser = new User("D", "d@b.com", "passD", "0004", UserStatus.ACTIVE);
+        User replyUser = new User("E", "e@b.com", "passE", "0005", UserStatus.ACTIVE);
+        Post post1 = new Post(postUser, "PostC1", "ContentC1");
+        em.persist(postUser);
+        em.persist(post1);
+        em.persist(replyUser);
+
+        DefaultResponse<ReplyDto> response = replyService.reply(-1L, post1.getId(),
+                "reply_content", List.of("i1", "i2", "i3"));
+        assertThat(response.getStatusCode()).isEqualTo(StatusCode.BAD_REQUEST);
+        assertThat(response.getResponseMessage()).isEqualTo(ResponseMessage.NOT_FOUND_USER);
+
+        assertThat(response.getData()).isNull();
+    }
+
+    @Test
+    void replyWrongPostFailed() {
+        User postUser = new User("D", "d@b.com", "passD", "0004", UserStatus.ACTIVE);
+        User replyUser = new User("E", "e@b.com", "passE", "0005", UserStatus.ACTIVE);
+        Post post1 = new Post(postUser, "PostC1", "ContentC1");
+        em.persist(postUser);
+        em.persist(post1);
+        em.persist(replyUser);
+
+        DefaultResponse<ReplyDto> response = replyService.reply(replyUser.getId(), -1L,
+                "reply_content", List.of("i1", "i2", "i3"));
+        assertThat(response.getStatusCode()).isEqualTo(StatusCode.BAD_REQUEST);
+        assertThat(response.getResponseMessage()).isEqualTo(ResponseMessage.NOT_FOUND_POST);
+
+        assertThat(response.getData()).isNull();
+    }
+
+    @Test
+    void replyLikeSuccess() {
         User postUser = new User("D", "d@b.com", "passD", "0004", UserStatus.ACTIVE);
         User replyUser = new User("E", "e@b.com", "passE", "0005", UserStatus.ACTIVE);
         User likeUser = new User("F", "f@b.com", "passF", "0006", UserStatus.ACTIVE);
@@ -77,9 +117,11 @@ class ReplyServiceTest {
         Reply reply = Reply.createReply(post1, replyUser, "reply_content");
         em.persist(reply);
 
-        Long replyLikeId = replyService.changeReplyLike(likeUser.getId(), reply.getId());
+        DefaultResponse<ReplyLikeDto> response = replyService.changeReplyLike(likeUser.getId(), reply.getId());
+        assertThat(response.getStatusCode()).isEqualTo(StatusCode.OK);
+        assertThat(response.getResponseMessage()).isEqualTo(ResponseMessage.REPLY_LIKE_CHANGE_SUCCESS);
 
-        ReplyLike replyLike = em.find(ReplyLike.class, replyLikeId);
+        ReplyLike replyLike = em.find(ReplyLike.class, response.getData().getId());
 
         // 좋아요 누른 사람 경험치 1 증가 검증
         assertThat(replyLike.getUser()).isEqualTo(likeUser);
@@ -93,7 +135,7 @@ class ReplyServiceTest {
     }
 
     @Test
-    void replyCancelLikeTest() {
+    void replyCancelLikeSuccess() {
         User postUser = new User("D", "d@b.com", "passD", "0004", UserStatus.ACTIVE);
         User replyUser = new User("E", "e@b.com", "passE", "0005", UserStatus.ACTIVE);
         User likeUser = new User("F", "f@b.com", "passF", "0006", UserStatus.ACTIVE);
@@ -118,8 +160,11 @@ class ReplyServiceTest {
         em.persist(replyLike);
 
         // 좋아요 취소 검증
-        Long replyLikeId = replyService.changeReplyLike(likeUser.getId(), reply.getId());
-        assertThat(replyLike.getId()).isEqualTo(replyLikeId);
+        DefaultResponse<ReplyLikeDto> response = replyService.changeReplyLike(likeUser.getId(), reply.getId());
+        assertThat(response.getStatusCode()).isEqualTo(StatusCode.OK);
+        assertThat(response.getResponseMessage()).isEqualTo(ResponseMessage.REPLY_LIKE_CHANGE_SUCCESS);
+
+        assertThat(replyLike.getId()).isEqualTo(response.getData().getId());
 
         likeUser = em.find(User.class, likeUser.getId());
 
@@ -134,8 +179,7 @@ class ReplyServiceTest {
     }
 
     @Test
-    @Rollback(false)
-    void editReplyTest() {
+    void editReplySuccess() {
         User postUser = new User("D", "d@b.com", "passD", "0004", UserStatus.ACTIVE);
         User replyUser = new User("E", "e@b.com", "passE", "0005", UserStatus.ACTIVE);
         Post post1 = new Post(postUser, "PostC1", "ContentC1");
@@ -156,8 +200,10 @@ class ReplyServiceTest {
         em.persist(replyImage3);
         em.persist(reply);
 
-        DefaultResponse<ReplyDto> response = replyService.editReply(replyUser.getId(), reply.getId(), "edited_content", List.of("i3", "i1", "i2"));
-
+        DefaultResponse<ReplyDto> response = replyService.editReply(replyUser.getId(), reply.getId(),
+                "edited_content", List.of("i3", "i1", "i2"));
+        assertThat(response.getStatusCode()).isEqualTo(StatusCode.OK);
+        assertThat(response.getResponseMessage()).isEqualTo(ResponseMessage.REPLY_EDIT_SUCCESS);
 
         Reply foundReply = em.find(Reply.class, response.getData().getId());
 
@@ -167,6 +213,42 @@ class ReplyServiceTest {
         assertThat(foundReply.getUser().getName()).isEqualTo("E");
         assertThat(foundReply.getPost().getContent()).isEqualTo(post1.getContent());
         assertThat(foundReply.getContent()).isEqualTo("edited_content");
+    }
+
+    @Test
+    void editReplyWrongUserFailed() {
+        User postUser = new User("D", "d@b.com", "passD", "0004", UserStatus.ACTIVE);
+        User replyUser = new User("E", "e@b.com", "passE", "0005", UserStatus.ACTIVE);
+        Post post1 = new Post(postUser, "PostC1", "ContentC1");
+        em.persist(postUser);
+        em.persist(post1);
+        em.persist(replyUser);
+
+        Reply reply = Reply.createReply(post1, replyUser, "reply_content");
+
+        ReplyImage replyImage1 = new ReplyImage("i1");
+        ReplyImage replyImage2 = new ReplyImage("i2");
+        ReplyImage replyImage3 = new ReplyImage("i3");
+
+        reply.setReplyImages(List.of(replyImage1, replyImage2, replyImage3));
+
+        em.persist(replyImage1);
+        em.persist(replyImage2);
+        em.persist(replyImage3);
+        em.persist(reply);
+
+        // 다른 유저로 댓글 수정 요청, 실패 response 객체 확인
+        DefaultResponse<ReplyDto> response = replyService.editReply(postUser.getId(), reply.getId(),
+                "edited_content", List.of("i3", "i1", "i2"));
+        assertThat(response.getStatusCode()).isEqualTo(StatusCode.BAD_REQUEST);
+        assertThat(response.getResponseMessage()).isEqualTo(ResponseMessage.NOT_SAME_USER);
+
+        // 안바뀐거 확인
+        Reply foundReply = replyRepository.findById(reply.getId()).get();
+        assertThat(foundReply.getImages()).extracting("path").containsExactly("i1", "i2", "i3");
+        assertThat(foundReply.getUser()).isEqualTo(replyUser);
+        assertThat(foundReply.getPost()).isEqualTo(post1);
+        assertThat(foundReply.getContent()).isEqualTo("reply_content");
     }
 
 
