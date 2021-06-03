fcd77b6a021a75388c8e4814be97b100f7460563
enjay27 <kdkm2002@gmail.com>
2021-06-03 12:11:30 +0900

    fix: UserController 위치 user 디렉터리로 변경

--- src/main/java/com/devin/dev/controller/user/UserController.java
+++ src/main/java/com/devin/dev/controller/user/UserController.java
@@ -1,14 +1,17 @@
-package com.devin.dev.controller;
+package com.devin.dev.controller.user;
 
 import com.devin.dev.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserRepository userRepository;
+
 
 
 }
