d5090ab2825de080ab759131dc547339271c4566
enjay27 <kdkm2002@gmail.com>
2021-06-03 12:10:57 +0900

    feat: 테스트 페이지 및 컨트롤러 작성

--- src/main/java/com/devin/dev/sample/HelloController.java
+++ src/main/java/com/devin/dev/sample/HelloController.java
@@ -1,23 +1,39 @@
 package com.devin.dev.sample;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     HelloService helloService;
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return "hello.html";
     }
 
     @GetMapping("/hello/delete/{id}")
     public String deleteHello(@PathVariable("id") Long id) {
         helloService.deleteHello(id);
         return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String getLoginForm() {
+        return "loginPage";
+    }
+
+    @GetMapping("/")
+    public String getIndex() {
+        return "index";
+    }
+
+    @GetMapping("/main")
+    public String getMain() {
+        return "main";
     }
 
 }
