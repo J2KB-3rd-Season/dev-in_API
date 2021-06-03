cd337cd88283e079e8a5d416ae8e1df077253a23
enjay27 <kdkm2002@gmail.com>
2021-06-03 17:46:04 +0900

    feat: HelloService 회원가입 메서드 추가

--- src/main/java/com/devin/dev/sample/HelloService.java
+++ src/main/java/com/devin/dev/sample/HelloService.java
@@ -1,13 +1,40 @@
 package com.devin.dev.sample;
 
+import com.devin.dev.model.DefaultResponse;
+import com.devin.dev.utils.ResponseMessage;
+import com.devin.dev.utils.StatusCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class HelloService {
-    HelloJpaRepository helloJpaRepository;
+    private final HelloJpaRepository helloJpaRepository;
+
+    private final HelloRepository helloRepository;
+
+    private final PasswordEncoder passwordEncoder;
 
     public void deleteHello(Long id) {
         Hello hello = helloJpaRepository.findById(id).get();
         helloJpaRepository.delete(hello);
     }
+
+    public DefaultResponse<HelloDto> signUp(String data, String password) {
+        Hello hello = Hello.builder()
+                        .username(data)
+                        .password(passwordEncoder.encode(password))
+                        .role("admin")
+                        .build();
+
+        helloRepository.save(hello);
+
+        HelloDto helloDto = new HelloDto(hello);
+
+        return new DefaultResponse<>(StatusCode.OK, ResponseMessage.CREATED_USER, helloDto);
+    }
+
 }
