57c93b67f8ce9765db54b87bdf4fa180e5eb7044
enjay27 <kdkm2002@gmail.com>
2021-06-03 17:45:26 +0900

    feat: HelloRepository 이름으로 유저 검색 쿼리 추가

--- src/main/java/com/devin/dev/sample/HelloRepository.java
+++ src/main/java/com/devin/dev/sample/HelloRepository.java
@@ -3,8 +3,13 @@ package com.devin.dev.sample;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 // 사용자 정의 쿼리 인터페이스를 extends 하여 사용한다.
 // 여기서는 메서드 이름만으로 만들어지는 쿼리만 선언한다.
 // 복잡한 쿼리는 HelloRepositoryQuery 혹은 다른 클래스를 만들어서 구현한다.
 public interface HelloRepository extends JpaRepository<Hello, Long>, HelloRepositoryQuery {
+
+    Optional<Hello> findByUsername(String username);
+
 }
