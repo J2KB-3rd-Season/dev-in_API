03813f430eea218be5c8996dc3c5e8b532a49c12
enjay27 <kdkm2002@gmail.com>
2021-06-03 12:14:02 +0900

    feat: 로그인 메서드에 활성 유저 체크 로직 추가 및 유저 상태변경 메서드 추가

--- src/main/java/com/devin/dev/service/UserService.java
+++ src/main/java/com/devin/dev/service/UserService.java
@@ -1,7 +1,9 @@
 package com.devin.dev.service;
 
+import com.devin.dev.dto.user.UserDetailsDto;
 import com.devin.dev.dto.user.UserSimpleDto;
 import com.devin.dev.entity.user.User;
+import com.devin.dev.entity.user.UserStatus;
 import com.devin.dev.model.DefaultResponse;
 import com.devin.dev.repository.user.UserRepository;
 import com.devin.dev.utils.ResponseMessage;
@@ -22,10 +24,10 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
-    public DefaultResponse<?> signUp(UserSimpleDto userDto) {
+    public DefaultResponse<UserSimpleDto> signUp(UserSimpleDto userDto) {
         // 엔티티 조회
         Optional<User> foundUser = userRepository.findByEmailEquals(userDto.getEmail());
-        if(foundUser.isPresent()) {
+        if (foundUser.isPresent()) {
             return new DefaultResponse<>(StatusCode.BAD_REQUEST, ResponseMessage.EXIST_USER_EMAIL);
         }
 
@@ -38,23 +40,53 @@ public class UserService {
         User user = new User(userDto);
         userRepository.save(user);
 
-        return new DefaultResponse<>(StatusCode.OK, ResponseMessage.CREATED_USER);
+        return new DefaultResponse<>(StatusCode.OK, ResponseMessage.CREATED_USER, userDto);
     }
 
     @Transactional(readOnly = true)
-    public DefaultResponse<?> signIn(UserSimpleDto userDto) {
-        // 엔티티 email 조회
-        Optional<User> foundUser = userRepository.findByEmailEquals(userDto.getEmail());
-        if(foundUser.isEmpty()) {
+    public DefaultResponse<UserDetailsDto> signIn(UserSimpleDto userDto) {
+        // DTO email 로 조회
+        Optional<UserDetailsDto> userOptional = userRepository.findUserDetailsByEmail(userDto.getEmail());
+        if (userOptional.isEmpty()) {
             return new DefaultResponse<>(StatusCode.BAD_REQUEST, ResponseMessage.NOT_FOUND_EMAIL);
         }
+        UserDetailsDto userDetailsDto = userOptional.get();
 
         // 비밀번호 체크
-        boolean passwordCheck = passwordEncoder.matches(userDto.getPassword(), foundUser.get().getPassword());
-        if(!passwordCheck) {
+        boolean passwordCheck = passwordEncoder.matches(userDto.getPassword(), userDetailsDto.getPassword());
+        if (!passwordCheck) {
             return new DefaultResponse<>(StatusCode.BAD_REQUEST, ResponseMessage.INCORRECT_PASSWORD);
+        }
+
+        // 활성 유저 체크
+        if (userDetailsDto.getStatus() != UserStatus.ACTIVE) {
+            return new DefaultResponse<>(StatusCode.BAD_REQUEST, ResponseMessage.INACTIVE_USER);
         }
 
         return new DefaultResponse<>(StatusCode.OK, ResponseMessage.LOGIN_SUCCESS);
     }
+
+    @Transactional
+    public DefaultResponse<UserDetailsDto> changeStatus(UserSimpleDto userDto, UserStatus userStatus) {
+        // 엔티티 조회
+        Optional<User> userOptional = userRepository.findByEmailEquals(userDto.getEmail());
+        if (userOptional.isEmpty()) {
+            return new DefaultResponse<>(StatusCode.BAD_REQUEST, ResponseMessage.NOT_FOUND_USER);
+        }
+        User user = userOptional.get();
+
+        // 비밀번호 체크
+        boolean passwordCheck = passwordEncoder.matches(userDto.getPassword(), user.getPassword());
+        if (!passwordCheck) {
+            return new DefaultResponse<>(StatusCode.BAD_REQUEST, ResponseMessage.INCORRECT_PASSWORD);
+        }
+
+        // 상태 변경
+        user.setStatus(userStatus);
+
+        UserDetailsDto userDetailsDto = new UserDetailsDto(user);
+
+        return new DefaultResponse<>(StatusCode.BAD_REQUEST, ResponseMessage.CHANGE_USER_STATUS, userDetailsDto);
+    }
+
 }
