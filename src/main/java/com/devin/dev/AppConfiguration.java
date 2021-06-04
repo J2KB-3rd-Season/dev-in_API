ef58ab153f8efe3962331af2b77b46dc853738a9
enjay27 <kdkm2002@gmail.com>
2021-06-04 23:30:37 +0900

    fix: 사용하지 않는 의존 패키지 제거

--- src/main/java/com/devin/dev/AppConfiguration.java
+++ src/main/java/com/devin/dev/AppConfiguration.java
@@ -1,16 +1,8 @@
 package com.devin.dev;
 
-import com.devin.dev.sample.BackedLoginService;
-import com.devin.dev.security.LoginSuccessHandler;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.EntityManager;
 
