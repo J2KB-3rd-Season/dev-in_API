47770e0fdd4aa8a05d364033bf5f678bb5694451
enjay27 <kdkm2002@gmail.com>
2021-06-04 15:23:24 +0900

    feat: Subject 이름으로 검색 메서드 추가

--- src/main/java/com/devin/dev/repository/subject/SubjectRepository.java
+++ src/main/java/com/devin/dev/repository/subject/SubjectRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
 
     List<Subject> findByNameIn(List<String> name);
+
+    Optional<Subject> findByName(String name);
 }
