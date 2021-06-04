1245a0a4008d4e2bf548d7c90e379f952af5b0ad
enjay27 <kdkm2002@gmail.com>
2021-06-04 15:24:33 +0900

    feat: SubjectService 생성 및 주제 생성 삭제 기능 추가

---
+++ src/main/java/com/devin/dev/service/SubjectService.java
@@ -0,0 +1,43 @@
+package com.devin.dev.service;
+
+import com.devin.dev.entity.post.Subject;
+import com.devin.dev.model.DefaultResponse;
+import com.devin.dev.repository.subject.SubjectRepository;
+import com.devin.dev.utils.ResponseMessage;
+import com.devin.dev.utils.StatusCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class SubjectService {
+
+    private final SubjectRepository subjectRepository;
+
+    @Transactional
+    public DefaultResponse<?> addSubject(String name) {
+        Optional<Subject> subjectOptional = subjectRepository.findByName(name);
+        if (subjectOptional.isPresent()) {
+            return new DefaultResponse<>(StatusCode.BAD_REQUEST, ResponseMessage.EXIST_SUBJECT);
+        }
+        Subject subject = new Subject(name);
+
+        subjectRepository.save(subject);
+
+        return new DefaultResponse<>(StatusCode.OK, ResponseMessage.CREATED_SUBJECT);
+    }
+
+    @Transactional
+    public DefaultResponse<?> removeSubject(String name) {
+        Optional<Subject> subjectOptional = subjectRepository.findByName(name);
+        if (subjectOptional.isEmpty()) {
+            return new DefaultResponse<>(StatusCode.BAD_REQUEST, ResponseMessage.NOT_FOUND_SUBJECT);
+        }
+        subjectRepository.delete(subjectOptional.get());
+
+        return new DefaultResponse<>(StatusCode.OK, ResponseMessage.DELETE_SUBJECT);
+    }
+}
