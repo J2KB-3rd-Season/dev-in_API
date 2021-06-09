8943e96c3ee7700ab24933da3efcc9eb60a09fe5
changmink <changmin043@gmail.com>
2021-06-10 02:57:40 +0900

    [#3] 게시판 목록 조회 기본 구현

--- src/main/java/com/devin/dev/security/SecurityConfiguration.java
+++ src/main/java/com/devin/dev/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.devin.dev.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -37,5 +38,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logout()
                     .logoutUrl("/logout")
                     .logoutSuccessUrl("/login");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/postlist/**");
     }
 }
